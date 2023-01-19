3057db648e8115d0c6e0a9fc1ecf59aaef9808f4
Bruno de Oliveira <historia.brunooliveira1993@gmail.com>
2023-01-19 17:12:24 -0300

    implemente fight monsters and PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,23 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  constructor(
+    private player1: Fighter,
+    private player2: Fighter,
+  ) {
+    super(player1);
+  }
+
+  figthing(): void {
+    while (this.player1.lifePoints > 0 && this.player2.lifePoints > 0) {
+      this.player1.attack(this.player2);
+      this.player2.attack(this.player1);
+    }
+  }
+
+  fight(): number {
+    this.figthing();
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
+}
--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -47,11 +47,11 @@ export default class Character implements Fighter {
     return this.lifePoints;
   }
 
-  attack(enemy: Fighter): void {
+  attack(enemy: Fighter | SimpleFighter): void {
     enemy.receiveDamage(this._strength);
   }
 
-  special?(enemy: Fighter): void {
+  special?(enemy: Fighter | SimpleFighter): void {
     enemy.receiveDamage(this._strength * this._lifePoints);
   }
 
